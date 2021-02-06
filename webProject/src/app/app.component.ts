f9f6e30d941deca09b5955a19ff5af9b8ecae8a3
adam_kh <adam.kohari@syncee.co>
2021-02-06 15:56:15 +0100

    menu and logo added

--- webProject/src/app/app.component.ts
+++ webProject/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'webProject';
+  getImgURL(): string {
+    return 'https://metromidsayap-water.gov.ph/wp-content/uploads/2016/11/Header-Background.jpg';
+  }
 }
--- webProject/src/app/app.module.ts
+++ webProject/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
